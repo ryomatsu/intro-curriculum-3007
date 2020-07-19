ece7706c439f5d4045ab276044d0035c73cd4857
Ryo Matsufuji <ryomatsu85@gmail.com>
2020-07-19 16:24:40 +0900

    solved

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 'use strict';
 
-module.exports = {
+function isMultipleOfSeventeen(number) {
+	//return numberOfArray.filter(number => (number % 17) === 0)
+	return (number % 17) === 0
 }
+
+module.exports = {isMultipleOfSeventeen}
